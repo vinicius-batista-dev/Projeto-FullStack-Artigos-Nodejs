3522a3cd9ac4e7f9e5bb221f3cf0c7264ff14994
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-05 21:39:31 -0300

    Criando 2 models artigos e categorias

---
+++ articles/Article.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const conn = require('../database/database');
+
+const Article = conn.define('articles', {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, slug:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+})
+
+module.exports = Article;
---
+++ categories/Category.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+const conn = require('../database/database');
+
+const Category = conn.define('categories', {
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }, slug:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+module.exports = Category;
