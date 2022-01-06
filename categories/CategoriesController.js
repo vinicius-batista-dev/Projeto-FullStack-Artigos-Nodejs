c0d1a42a3d83ad71280f7d59b3b565d6aca741a8
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-05 22:35:28 -0300

    Configurando admin

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-    res.send("Rota de categorias");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-    res.send("ROTA PARA CRIAR UMA NOVA CATEGORIA")
+    res.render("admin/categories/new")
 });
 
 module.exports = router;
