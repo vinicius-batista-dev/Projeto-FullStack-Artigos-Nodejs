faf1830180462cbc5859928c0a28907034841ce5
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-07 18:49:47 -0300

    Consertando o error da tabela artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,32 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
-const Article = require('./Article');
+const Article = require('../articles/Article');
 const slugify = require('slugify');
 
 router.get('/admin/articles', (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("admin/articles/index", {articles: articles})
+    Article.findAll({
+        //Busca por categorias
+        include: [{model: Category}]
+    }).then(articles => {
+        res.render("admin/articles/index", {articles});
     })
 });
 
 router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then(categories => {
-        res.render('admin/articles/new',{categories: categories});
+        res.render('admin/articles/new',{categories});
     })
 });
 
-router.post("/admin/articles/save",  (req, res) => {
-    const {title, body, category} = req.body;
-        
+router.post("/articles/save", (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const category = req.body.category;
+
     Article.create({
         title: title,
         slug: slugify(title),
         body: body,
         categoryId: category
     }).then(() => {
-        res.redirect("/admin/articles");
+        res.redirect('/admin/articles');
     });
+
+})
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            //deletar uma categoria
+              Article.destroy({
+                  where: {
+                      id: id
+                  }
+              }).then(() => {
+                  res.redirect("/admin/articles");
+              })
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }else{
+        res.redirect("/admin/articles");
+    }
 });
 
 module.exports = router;
