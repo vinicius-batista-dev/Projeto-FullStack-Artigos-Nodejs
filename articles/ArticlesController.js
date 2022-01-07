c2066d51bc463f7cce12794f51c6579616ccbdce
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-06 23:30:59 -0300

    Salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const slugify = require('slugify');
 
 router.get("/articles", (req, res) => {
     res.send("Rota de artigos");
@@ -11,5 +12,19 @@ router.get("/admin/articles/new", (req, res) => {
         res.render("admin/articles/new", {categories: categories})
     })
 });
+
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        //chave estrangeira que faz referencia a um elemento de uma tabela
+        categoryId: category
+    })
+})
 
 module.exports = router;
