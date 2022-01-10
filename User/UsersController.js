3ea1e0841eb6a9ac6016e8c4f3f9ab47ae453a8b
vinicius batista braz <20201012000256@pucgo.edu.br>
2022-01-10 15:26:27 -0300

    Finalizando o projeto

--- User/UsersController.js
+++ User/UsersController.js
@@ -69,6 +69,11 @@ router.post("/authenticate", (req, res) => {
     });
 });
 
+router.get("/logout", (req, res) => {
+    req.session.user = undefined;
+    res.redirect("/");
+})
+
 
 
 module.exports = router;
